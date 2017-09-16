78cb5d3f2f1af1dc0423fb37abc032a8bd45ff7b
Nathan Tippy <nathan@tippy.name>
2017-09-15 19:12:28 -0500

    Starting new project

---
+++ src/main/java/com/ociweb/neuralIoT/VisualStageFactory.java
@@ -0,0 +1,27 @@
+package com.ociweb.neuralIoT;
+
+import com.ociweb.pronghorn.neural.StageFactory;
+import com.ociweb.pronghorn.pipe.Pipe;
+import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+
+public class VisualStageFactory implements StageFactory {
+
+	@Override
+	public void newStage(GraphManager gm, Pipe[] input, Pipe output) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newStage(GraphManager gm, Pipe input, Pipe[] output) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newStage(GraphManager gm, Pipe[] input, Pipe[] output) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
