dfe9a15c77d2dabe7a4126e7b75268f4ee6dbe7f
Bryson Hunsaker <brysonhunsaker@gmail.com>
2017-11-05 23:51:23 -0600

    added test file reading and some N building stuff based on tutorials

--- OAPN/src/main/java/nniot/oapn/OAPNnet.java
+++ OAPN/src/main/java/nniot/oapn/OAPNnet.java
@@ -5,13 +5,88 @@
  */
 package nniot.oapn;
 
+import com.ociweb.pronghorn.neural.NeuralGraphBuilder;
+import com.ociweb.pronghorn.neural.StageFactory;
+import com.ociweb.pronghorn.pipe.MessageSchemaDynamic;
+import com.ociweb.pronghorn.pipe.Pipe;
+import com.ociweb.pronghorn.pipe.SchemalessFixedFieldPipeConfig;
+import com.ociweb.pronghorn.stage.scheduling.GraphManager;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author nick
+ * @author bryson
  */
 public class OAPNnet {
+    static final int numAttributes = 10;
+    static final int numTestRecords = 100;
+    static final int numTrainingRecords = 50;
+    static final String testDataFN = "";
+    static final String trainingDataFN = "";
+        
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String [][] trainingData = new String[numTestRecords][numAttributes + 1];
+        String [][] testingData =  new String[numTrainingRecords][numAttributes];
+        //String []   trainingAnswers = new String[numTrainingRecords];
+        
+        readInData(trainingData, trainingDataFN);
+        readInData(testingData, testDataFN);
+        
+        GraphManager gm = new GraphManager();
+        GraphManager.addDefaultNota(gm, GraphManager.SCHEDULE_RATE, 20_000);
+        
+        
     }
     
+    public static void readInData(String[][] data, String fn){
+        String line = null;
+        try {
+            int i = 0;
+            FileReader fileReader = new FileReader(fn);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                data[i] = line.split(",");
+                i++;
+            }   
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file:" + fn);            
+        }
+        catch(IOException ex) {
+            System.out.println("IOException while reading file:" + fn);                
+        }
+    }
+    
+    //Incomplete, currently based on Nathan's tutorial
+    public static void buildVisualNeuralNet(GraphManager gm) {
+		final SchemalessFixedFieldPipeConfig config = new SchemalessFixedFieldPipeConfig(32);
+		config.hideLabels();
+		
+		final StageFactory<MessageSchemaDynamic> factory = new VisualStageFactory();
+		
+		
+		int inputsCount = 5;		
+		Pipe<MessageSchemaDynamic>[] prevA = Pipe.buildPipes(inputsCount, config);
+		
+                //TODO: refer to instance of our stage here
+		//DataProducerStage.newInstance(gm, prevA);
+			
+		int nodesInLayerA = 3;
+		Pipe<MessageSchemaDynamic>[][] fromA = NeuralGraphBuilder.buildPipeLayer(gm, config, prevA, nodesInLayerA, factory);
+
+		int nodesInLayerB = 5;
+		Pipe<MessageSchemaDynamic>[][] fromB = NeuralGraphBuilder.buildPipeLayer(gm, config, fromA, nodesInLayerB, factory);
+		
+		Pipe<MessageSchemaDynamic>[] fromC = NeuralGraphBuilder.lastPipeLayer(gm, fromB, factory);
+			
+                //TODO: refer to instance of our stage here
+		//DataConsumerStage.newInstance(gm, fromC, target);
+		
+	}
 }
