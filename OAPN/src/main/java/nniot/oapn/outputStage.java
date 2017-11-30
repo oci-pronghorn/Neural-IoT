e6ad585a7a9a41940db5e2531e410264b4109b29
Nick K <local>
2017-11-30 01:38:40 -0600

    refactored and expanded the output stage

--- OAPN/src/main/java/nniot/oapn/outputStage.java
+++ OAPN/src/main/java/nniot/oapn/outputStage.java
@@ -12,33 +12,71 @@ import com.ociweb.pronghorn.stage.PronghornStage;
 import static com.ociweb.pronghorn.stage.PronghornStage.NONE;
 import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class outputStage extends PronghornStage {
 
     private final Pipe<MessageSchemaDynamic>[] output;
-    private final File outputPath;
-
-    public static outputStage newInstance(GraphManager gm, String[][] data, Pipe<MessageSchemaDynamic>[] output, String fname) {
+    private  PrintWriter outputFileWriter;
+    private File trainingFile;
+    private final String[][] data;
+    public static outputStage newInstance(GraphManager gm, String[][] data, Pipe<MessageSchemaDynamic>[] output, String fname) throws FileNotFoundException {
         return new outputStage(gm, data, output,fname);
     }
 
-    public outputStage(GraphManager gm, String[][] data, Pipe<MessageSchemaDynamic>[] output, String fname) {
+    public outputStage(GraphManager gm, String[][] data, Pipe<MessageSchemaDynamic>[] output, String fname) throws FileNotFoundException {
         super(gm, NONE, output);
         this.output = output;
-        this.outputPath=new File
+        this.outputFileWriter= new PrintWriter(new File(fname));
+        trainingFile=new File(fname.concat("OUTPUT"));
+        this.data=data;
+        
     }
 
     public void run(){
-
+        writeOutput();
         
     }
     
-    public int writeOutput() {
+   /* public int writeOutput() {
 		int result = Integer.MAX_VALUE;
 		int i = output.length;
 		while (--i>=0) {
 			result = Math.min(result, SchemalessPipe.roomRemaining(output[i]));
 		}
 		return result;
+    }*/
+    public void writeOutput() throws FileNotFoundException{
+        for (String[] row : data) {
+           String s =""; 
+            for (String item : row) {
+                s+=item;
+                s+=", ";
+            }
+            s+= getCorrelatedOutput(row);
+            s+="\n";
+            outputFileWriter.write(s);
+        }
+        if (OAPNnet.isTraining) {
+            outputFileWriter=new PrintWriter(trainingFile);
+            for (String name: OAPNnet.weightsMap.keySet(){
+
+            String key =name.toString();
+            String value = example.get(name).toString();  
+            System.out.println(key + " " + value);  
+
+
+} 
+
+            
+            
+        }
+        
+    }
+    private String getCorrelatedOutput(String s[]){
+        // ask dr mayer and mr tippy
+        return null;
     }
 }
